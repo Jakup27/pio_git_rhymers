f41b55d1f170ffafe9361708394c8f3c2a09ee3f
Jakup27 <240799@edu.p.lodz.pl>
2023-05-09 17:44:41 +0200

    17. Szkielet dokumentacji do klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,30 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa HanoiRhymer dziedziczy po klasie DefaultCountingOutRhymer
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    private int totalRejected = 0;
+    /**
+     * Stała INT1
+     */
+    public static final int INT1 = 0;
+    /**
+     * Zmienna typu int, która przechowuje ilość odrzuconych wyliczeń
+     */
+    int totalRejected = INT1;
 
-    public int getTotalRejected() {
-        return totalRejected;
-    }
-
+    /**
+     * @return zwraca ilość odrzuconych wyliczeń
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * @param in - liczba, którą odkładamy na stosie
+     *           metoda odkłada parametr in na stos
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
